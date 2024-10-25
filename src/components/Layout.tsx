26e2fd4af3fd86f1f1ac7ef4e80377b3a1efb6de
Ahsan Ellahi <mail.ahsan.ellahi@gmail.com>
2024-10-25 21:13:57 +0500

    update: table ui

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,7 +4,7 @@ import Sidebar from './Sidebar';
 
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
     return (
-        <div className="min-h-screen bg-gray-300">
+        <div className="min-h-screen bg-white">
             <Header />
             <div className="flex pt-16"> {/* Add padding to prevent content overlap */}
                 {/* Sidebar */}
